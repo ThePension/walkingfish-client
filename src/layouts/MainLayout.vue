9417ea5da0e38f8fe7143643281ed713c6c59a07
Neil <neil.benoit@he-arc.ch>
2023-04-14 18:54:07 +0200

    Contact, footer and small changes

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,5 +1,5 @@
 <template>
-  <q-layout view="lHh Lpr lFf">
+  <q-layout view="hHh LpR fff">
     <q-header>
       <q-toolbar>
 
@@ -19,14 +19,31 @@
           to="/catalog">
         </q-btn>
 
-        <q-btn 
-          flat 
-          label="Contact"
-          to="/about">
-        </q-btn>
+        <q-btn-dropdown label="Contact" flat>
+          <q-list>
+            <q-item clickable v-close-popup>
+              <a href="https://instagram.com/walkingxfish">
+              </a>
+              <q-item-section avatar>
+                <q-avatar>
+                  <img src="../statics/Instagram_icon.png"/>
+                </q-avatar>
+              </q-item-section>
+              <q-item-section>
+                <q-item-label>Instagram</q-item-label>
+              </q-item-section>
+            </q-item>
+          </q-list>
+        </q-btn-dropdown>
 
       </q-toolbar>      
     </q-header>
+
+    <q-footer flat>
+      <q-toolbar>
+        <q-toolbar-title class="text-center">© 2023 Copyright: WalkingFish.ch</q-toolbar-title>
+      </q-toolbar>
+    </q-footer>
 
     <q-page-container>
       <router-view />
